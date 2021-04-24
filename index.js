781db60150512dc14b4f379b4ba6c812ea769505
Tomi <tomi.taskila@tyomaa.com>
2021-04-24 17:39:12 +0300

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const { request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('post-data', (req, res) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 let persons = [
   {

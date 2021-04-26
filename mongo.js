c22bd09dfcd467aa67a64ca6ce4d41bbcb7bd196
Tomi <tomi.taskila@tyomaa.com>
2021-04-26 20:04:04 +0300

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.jusrt.mongodb.net/phonebook-app?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if ( name && number ) {
+  const person = new Person({
+    name,
+    number,
+  })
+
+  person.save().then(person => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(results => {
+    console.log('phonebook:')
+    results.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+

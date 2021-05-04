b3b858b55a209303dceea19894b8ed28593db67e
Tomi <tomi.taskila@tyomaa.com>
2021-05-04 19:40:32 +0300

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -75,10 +75,12 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
